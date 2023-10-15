ab5399971ebdd10861fdd15ac4ba8d860f207c8d
DatePest <108296950+DatePest@users.noreply.github.com>
2023-10-15 22:15:56 +0900

    重構輩分

--- Assets/Scipts/CardDeckCustom/CardDeckCustom_CardCheckBase.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class CardDeckCustom_CardCheckBase 
-{
-    public abstract bool UseCheck(SO_CardBase sO);
-}
--- Assets/Scipts/XXX/Scenes_View.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
-
-public class Scenes_View : MonoBehaviour
-{
-    public event Action<int> UpGUIData;
-}
